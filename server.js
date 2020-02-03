f10dfaf022e4c3944107d7c153df2375181732e1
burgerjang <rozen8831@gmail.com>
2020-02-03 04:24:07 +0000

    README.md test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//const request = require('request');
+const request = require('request');
 
-//const url = "https://golang.org/dl/";
+const url = "https://golang.org/dl/";
 
 request(url, (error, response, body) => {
   if (error) throw error;
