002f7415c0ff6294c65ad50d3ce5bc641e627a54
burgerjang <rozen8831@gmail.com>
2020-02-03 03:58:56 +0000

    Add select list logic

--- install_go.sh
+++ install_go.sh
@@ -1,42 +1,69 @@
 #!/bin/bash
 
-apt update
+command -v npm
 
-apt-get -y install nodejs
+if [ $? -eq 1 ]; then
+	apt update
 
-apt-get -y install npm
+	apt-get -y install nodejs
 
-npm install request --save
+	apt-get -y install npm
+
+	npm install request --save
+fi
+
+command -v fzf
+
+if [ $? -eq 1 ]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+	source ~/.bashrc
+	FZF=true
+else
+	FZF=true
+fi
 
 CHECK_GO=$(go version  2>&1 > /dev/null)
 RESULT=$?
 
+exp="go([[:alnum:]\.]*)\.src.*"
+
 if [ ! $RESULT -eq 0 ]; then
 
-  if [ ! -f log ]; then
-    npm start server.js > log
-  fi
-  GO_URL=$(cat log | grep go1.13.5 | grep linux-amd64 | sed -n 1p | awk -F' ' '{print $4}' | awk -F'"' '{print $2}')
-  GO_BINARY=$(echo $GO_URL | awk -F'/' '{print $5}')
+	if [ ! -f log ]; then
+		npm start server.js > log
+	fi
 
-  wget ${GO_URL}
-  tar -C /usr/local -xvf ${GO_BINARY}
+	if [ "${FZF}" == "true" ]; then
+		while read -r line; do
+			if [[ "${line}" =~ ${exp} ]]; then
+				echo "${BASH_REMATCH[1]}"
+			fi
+		done < log | sort --version-sort --reverse | uniq > list
+		SELECTED=$(cat list | fzf --reverse --no-mouse --height=15 --bind=left:page-up,right:page-down)
+	fi
+
+	GO_URL=$(cat log | grep "go${SELECTED}" | grep linux-amd64 | sed -n 1p | awk -F' ' '{print $4}' | awk -F'"' '{print $2}')
+	GO_BINARY=$(echo $GO_URL | awk -F'/' '{print $5}')
+	wget ${GO_URL}
+	tar -C /usr/local -xvf ${GO_BINARY}
 fi
 
-FIND_GO=$(cat ~/.bashrc | grep "go/bin" | wc -l)
-if [ ${FIND_GO} -eq 0 ]; then
-  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-fi
 
-FIND_GOPATH=$(cat ~/.bashrc | grep "GOPATH" | wc -l)
-if [ ${FIND_GOPATH} -eq 0 ]; then
-  echo 'export GOPATH=$(go env GOPATH)' >> ~/.bashrc
-fi
+#FIND_GO=$(cat ~/.bashrc | grep "go/bin" | wc -l)
+#if [ ${FIND_GO} -eq 0 ]; then
+#	echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+#fi
 
-FIND_GOROOT=$(cat ~/.bashrc | grep "GOROOT" | wc -l)
-if [ ${FIND_GOROOT} -eq 0 ]; then
-  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-fi
+#FIND_GOPATH=$(cat ~/.bashrc | grep "GOPATH" | wc -l)
+#if [ ${FIND_GOPATH} -eq 0 ]; then
+#	echo 'export GOPATH=$(go env GOPATH)' >> ~/.bashrc
+#fi
 
-echo -e 'source ~/.bashrc'
-echo -e
+#FIND_GOROOT=$(cat ~/.bashrc | grep "GOROOT" | wc -l)
+#if [ ${FIND_GOROOT} -eq 0 ]; then
+#	echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+#fi
+
+#echo -e 'source ~/.bashrc'
+##echo -e
